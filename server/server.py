033d97cb32c752b3e6d9e8a09aac708763c95908
Maxwell Nemec <maxnemec@gmail.com>
2022-01-18 10:24:31 -0500

    changed import paths in server.py

--- server/server.py
+++ server/server.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 
 
 
-from nutrikit.server.api import *
-from nutrikit.server.api.resources import *
+from server.api import *
+from server.api.resources import *
 
 app = Flask(__name__, static_folder='mjn7597-react/build', static_url_path='') #create Flask instance
 
